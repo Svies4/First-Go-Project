6f2ec9cd8d6ef11913eba5266115eaa4a237121c
Svies4 <118697861+Svies4@users.noreply.github.com>
2023-01-11 01:57:16 +0200

    finally working logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module First-Go-Project
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0
--- service.go
+++ service.go
@@ -2,34 +2,44 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"github.com/gorilla/mux"
 	"net/http"
+	"sync"
+	_ "sync"
 	"time"
 )
 
-var msgChan chan string
+// var msgChan chan string
+var msgChanMap = make(map[string]chan string)
+var mut = &sync.Mutex{}
 
-func getTime(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+func addMsg() {
 
-	if msgChan != nil {
-		msg := time.Now().Format("15:04:05")
-		msgChan <- msg
-	}
 }
 
 func sseHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	topic, okk := vars["topic"]
+	fmt.Println(`topic := `, topic)
+
+	if !okk {
+		fmt.Println("id is missing in parameters")
+	}
 	fmt.Println("Client connected")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
-	msgChan = make(chan string)
+	//msgChan = make(chan string)
+	//msgChanMap := make(map[string]chan string)
+
+	//msgChanMap[topic] = make(chan string, 2)
+	//msgChanMap[topic] <- topic
 
 	defer func() {
-		close(msgChan)
-		msgChan = nil
+		close(msgChanMap[topic])
+		msgChanMap[topic] = nil
 		fmt.Println("Client closed connection")
 	}()
 
@@ -40,7 +50,7 @@ func sseHandler(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		select {
-		case message := <-msgChan:
+		case message := <-msgChanMap[topic]:
 			fmt.Println("case message... sending message")
 			fmt.Println(message)
 			fmt.Fprintf(w, "data: %s\n\n", message)
@@ -49,21 +59,85 @@ func sseHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Client closed connection")
 			return
 		}
+		//time.Sleep(300 * time.Millisecond)
+		//fmt.Println("WAITING")
+		//msgChanMap[topic] <- topic
 	}
 
 }
 
+type Topic struct {
+	Name     string
+	Messages []Message
+}
+type Message struct {
+	Id       int
+	Contents string
+	Date     time.Time
+}
+
 func main() {
+	msgChanMap["general"] = make(chan string, 5)
+	r := mux.NewRouter()
+	r.Handle("/", http.Handler(http.FileServer(http.Dir("./"))))
+	//r.HandleFunc("/infocenter/sa", sseHandler)
+	r.HandleFunc("/infocenter/{topic}", Provisions)
 
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/event", sseHandler)
-	http.HandleFunc("/time", getTime)
+	go func() {
+		fmt.Println("serving on 8080")
+		err := http.ListenAndServe(":8080", r)
+		if err != nil {
+			panic("ListenAndServe: " + err.Error())
+		}
+	}()
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	/*go func() {
+		for {
+			//time.Sleep(300 * time.Millisecond)
+			//msgChanMap := make(map[string]chan string)
+			msgChanMap["general"] = make(chan string, 2)
+			//mut.Lock()
+			msgChanMap["general"] <- "GENERALmessage"
+			//mut.Unlock()
+			fmt.Println("id is missing in parameters")
+		}
+	}()*/
+	go func() {
+		fmt.Println("a")
+	}()
+	select {}
+}
+
+func Provisions(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	topic, ok := vars["topic"]
+	if !ok {
+		fmt.Println("id is missing in parameters")
+	}
+	fmt.Println(`topic := `, topic)
+
+	/*fmt.Println(`topic cvfvsv:= `, msgChanMap["topic"])
+	if msgChanMap["topic"] != nil {
+		msg := "topic"
+		msgChanMap["topic"] <- msg
+	}*/
+	switch r.Method {
+	case "GET":
+
+		sseHandler(w, r)
+	case "POST":
+		fmt.Println("aaaaaaaaaaaaaaaaaaaaaaaa")
+		mut.Lock()
+		//time.Sleep(1000 * time.Millisecond)
+
+		msgChanMap["general"] <- "Gdassadasdasdssage"
+		mut.Unlock()
+		fmt.Println("id is missing in parameters")
+
+	default:
+		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
 
-	fmt.Printf("asasasasas")
+	//call http://localhost:8080/provisions/someId in your browser
+	//Output : id := someId
 }
