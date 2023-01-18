2944ba6d1bc1eef3d2166c5d7c6e822b7d34b700
Svies4 <118697861+Svies4@users.noreply.github.com>
2023-01-18 18:13:22 +0200

    Done

--- service.go
+++ service.go
@@ -7,9 +7,12 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 )
 
 var IDs = make(map[int]bool)
+var Topics = make(map[string]bool)
+var Brokers = make(map[string]*Broker)
 
 type Broker struct {
 	Topic          string
@@ -19,11 +22,10 @@ type Broker struct {
 	clients        map[chan []byte]bool
 }
 
-/*type Messagee struct {
-	Id       int
-	Contents string
-	Date     time.Time
-}*/
+type Message struct {
+	Message string `json:"msg"`
+	ID      int    `json:"id"`
+}
 
 func NewServer(Topic string) (broker *Broker) {
 	// Instantiate a broker
@@ -35,15 +37,9 @@ func NewServer(Topic string) (broker *Broker) {
 		Topic:          Topic,
 	}
 
-	// Set it running - listening and broadcasting events
 	go broker.listen()
 
 	return
-}
-
-type Message struct {
-	Message string `json:"msg"`
-	ID      int    `json:"id"`
 }
 
 func (broker *Broker) Stream(w http.ResponseWriter, r *http.Request) {
@@ -59,19 +55,23 @@ func (broker *Broker) Stream(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	// Each connection registers its own message channel with the Broker's connections registry
 	messageChan := make(chan []byte)
 
-	// Signal the broker that we have a new connection
+	connectionDate := time.Now()
+
 	broker.newClients <- messageChan
 
-	// Remove this client from the map of connected clients
-	// when this handler exits.
 	defer func() {
 		broker.closingClients <- messageChan
 	}()
 
-	// Listen to connection close and un-register messageChan
+	go func() {
+		time.Sleep(30 * time.Second)
+		fmt.Fprintf(w, "event: timeout\ndata: %s\n\n", fmt.Stringer(time.Since(connectionDate)))
+		flusher.Flush()
+		broker.closingClients <- messageChan
+	}()
+
 	notify := w.(http.CloseNotifier).CloseNotify()
 
 	go func() {
@@ -80,12 +80,7 @@ func (broker *Broker) Stream(w http.ResponseWriter, r *http.Request) {
 	}()
 
 	for {
-
-		// Write to the ResponseWriter
-		// Server Sent Events compatible
 		fmt.Fprintf(w, "data: %s\n\n", <-messageChan)
-
-		// Flush the data immediatly instead of buffering it for later.
 		flusher.Flush()
 	}
 
@@ -96,21 +91,19 @@ func (broker *Broker) listen() {
 		select {
 		case s := <-broker.newClients:
 
-			// A new client has connected.
-			// Register their message channel
 			broker.clients[s] = true
 			log.Printf("Client added. %d registered clients", len(broker.clients))
 		case s := <-broker.closingClients:
 
-			// A client has dettached and we want to
-			// stop sending them messages.
 			delete(broker.clients, s)
 			log.Printf("Removed client. %d registered clients", len(broker.clients))
+			if len(broker.clients) == 0 {
+				delete(Topics, broker.Topic)
+				return
+			}
 		case event := <-broker.Notifier:
 
-			// We got a new event from the outside!
-			// Send event to all connected clients
-			for clientMessageChan, _ := range broker.clients {
+			for clientMessageChan := range broker.clients {
 				clientMessageChan <- event
 			}
 		}
@@ -119,58 +112,32 @@ func (broker *Broker) listen() {
 }
 
 func (broker *Broker) BroadcastMessage(w http.ResponseWriter, r *http.Request) {
-	// params := mux.Vars(r)
-
 	var msg Message
-	fmt.Println(json.NewDecoder(r.Body).Decode(&msg))
+
 	msg.ID = GenerateId()
 	_ = json.NewDecoder(r.Body).Decode(&msg)
-
-	//broker.Notifier <- []byte(fmt.Sprintf("the time is %v", msg))
 	j, _ := json.Marshal(msg)
-	//fmt.Println(r)
+
 	broker.Notifier <- []byte(j)
-	json.NewEncoder(w).Encode(msg)
-	//broker.Notifier <- []byte("aaaa")
+	//json.NewEncoder(w).Encode(msg)
 
 }
 
 func GenerateId() int {
 	for {
-		var ran = rand.Intn(99999999-9999999) + 9999999
+		var ran = rand.Intn(99999-9999) + 9999
 		if IDs[ran] == false {
 			IDs[ran] = true
-			fmt.Println(ran)
 			return ran
 		}
 	}
 }
-func main() {
-	//broker := NewServer()
-	router := mux.NewRouter()
-
-	router.Handle("/", http.Handler(http.FileServer(http.Dir("./"))))
-
-	//router.HandleFunc("/messages", broker.BroadcastMessage).Methods("POST")
-
-	router.HandleFunc("/infocenter/{topic}", Provisions)
-
-	log.Fatal(http.ListenAndServe(":8080", router))
-
-}
-
-var Topics = make(map[string]bool)
-var Brokers = make(map[string]*Broker)
-
-//var Servers = make(map[Broker]New)
-
-func Provisions(w http.ResponseWriter, r *http.Request) {
+func RequestHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	topic, ok := vars["topic"]
 	if !ok {
 		fmt.Println("id is missing in parameters")
 	}
-	fmt.Println(`topic := `, topic)
 
 	switch r.Method {
 	case "GET":
@@ -197,4 +164,15 @@ func Provisions(w http.ResponseWriter, r *http.Request) {
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.Handle("/", http.Handler(http.FileServer(http.Dir("./"))))
+
+	router.HandleFunc("/infocenter/{topic}", RequestHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
